166754ff7251b3a2f8aeef046a4157cf4bacab31
Erik Bryn <ebryn@marketmonitors.com>
2010-09-21 18:47:58 -0700

    Cleaned up tests

--- test/test_freetds.rb
+++ test/test_freetds.rb
@@ -9,32 +9,28 @@ class TestFreeTDS < Test::Unit::TestCase
       :username => 'sa',
       :password => ''
     }
+    @driver = FreeTDS::Driver.new
+    assert_not_nil(@driver, "driver creation failed")
+    
+    @connection = @driver.connect(@config)
+
+    @connection.statement('drop database freetds_test').execute    
+    @connection.statement('create database freetds_test').execute
+    @connection.statement('use freetds_test').execute
   end
   
-  def test_driver
-    driver = FreeTDS::Driver.new
-    assert_not_nil(driver, "driver creation failed")
-    
-    connection = driver.connect(@config)
+  def teardown
+    @connection.close
+  end
+  
+  def test_basics
+    @connection.statement('create table posts ( id int, name varchar(100), body text, post_date datetime)').execute
+    @connection.statement("insert into posts values (1, 'Foo', 'This is a test message', getdate())").execute
+    @connection.statement("insert into posts values (2, 'Bar', 'This is another test message', getdate())").execute
 
-    begin
-      connection.statement('drop database freetds_test').execute      
-    rescue  
-    end
-    
-    connection.statement('create database freetds_test').execute
-      
-    connection.statement('use freetds_test').execute
-    connection.statement('create table posts ( id int, name varchar(100), body text, post_date datetime)').execute
-    connection.statement("insert into posts values (1, 'Foo', 'This is a test message', getdate())").execute
-    connection.statement("insert into posts values (2, 'Bar', 'This is another test message', getdate())").execute
+    @connection.statement("SET TEXTSIZE #{1024*1024*1024}").execute
 
-    connection.statement('create table float_test ( num float )').execute
-    connection.statement("insert into float_test values ( 1.75 )").execute
-
-    connection.statement("SET TEXTSIZE #{1024*1024*1024}").execute
-
-    statement = connection.statement('select * from posts')
+    statement = @connection.statement('select * from posts')
     statement.execute
 
     # puts statement.rows[0].inspect
@@ -54,67 +50,84 @@ class TestFreeTDS < Test::Unit::TestCase
       assert_not_nil(col['name'])
       assert(row.has_key?(col['name']), "Column #{col[name]} missing from selected row")
     end
-    
-    bad_statement = connection.statement('this should fail')
+  end
+  
+  def test_bad_statement
+    bad_statement = @connection.statement('this should fail')
     assert_raise(IOError, "The statement execution should have failed with an IOError") {
       bad_statement.execute
     }
-    
-    float_statement = connection.statement('select * from float_test')
+  end
+  
+  def test_floats
+    @connection.statement('create table float_test ( num float )').execute
+    @connection.statement("insert into float_test values ( 1.75 )").execute
+
+    float_statement = @connection.statement('select * from float_test')
     float_statement.execute
     row = float_statement.rows.first
     assert_equal(1.75, row["num"], "floats should match")
+  end
+  
+  def test_large_text_fields
+    @connection.statement('create table text_field_test ( id int, data text )').execute
+    @connection.statement("insert into text_field_test values ( 1, '#{'a'*20}' )").execute
+    @connection.statement("insert into text_field_test values ( 2, '#{'a'*500}' )").execute
+    @connection.statement("insert into text_field_test values ( 3, '#{'a'*5000000}' )").execute
     
-    
-    connection.statement('create table text_field_test ( id int, data text )').execute
-    connection.statement("insert into text_field_test values ( 1, '#{'a'*20}' )").execute
-    connection.statement("insert into text_field_test values ( 2, '#{'a'*500}' )").execute
-    connection.statement("insert into text_field_test values ( 3, '#{'a'*5000000}' )").execute
-    
-    text_statement = connection.statement('select * from text_field_test where id = 1')
+    text_statement = @connection.statement('select * from text_field_test where id = 1')
     text_statement.execute
     row = text_statement.rows.first
     assert_equal('a'*20, row["data"], "data should match")
-    text_statement = connection.statement('select * from text_field_test where id = 2')
+    text_statement = @connection.statement('select * from text_field_test where id = 2')
     text_statement.execute
     row = text_statement.rows.first
     assert_equal('a'*500, row["data"], "data should match")
-    text_statement = connection.statement('select * from text_field_test where id = 3')
+    text_statement = @connection.statement('select * from text_field_test where id = 3')
     text_statement.execute
     row = text_statement.rows.first
     assert_equal('a'*5000000, row["data"], "data should match")
+  end
 
+  def test_identity_insert
     # identity insert testing
-    connection.statement('create table identity_insert_test ( id int identity, data text )').execute
-    connection.statement("SET IDENTITY_INSERT identity_insert_test ON; insert into text_field_test values ( 1, '#{'a'*20}' ); SET IDENTITY_INSERT identity_insert_test OFF").execute
+    @connection.statement('create table identity_insert_test ( id int identity, data text )').execute
+
+    assert_nothing_raised {
+      @connection.statement("SET IDENTITY_INSERT identity_insert_test ON").execute
+      @connection.statement("INSERT INTO identity_insert_test (id, data) VALUES ( 1, '#{'a'*20}' )").execute
+      @connection.statement("SET IDENTITY_INSERT identity_insert_test OFF").execute
+    }
+  end
+  
+  def test_previous_inserted_id  
+    @connection.statement('create table posts ( id int, name varchar(100), body text, post_date datetime)').execute
     
     # get previously inserted record's id
-    connection.statement("insert into posts values (4, 'Another', 'This is yet another test message', getdate())").execute
-    stmt = connection.statement("SELECT SCOPE_IDENTITY() AS Ident")
+    @connection.statement("insert into posts values (4, 'Another', 'This is yet another test message', getdate())").execute
+    stmt = @connection.statement("SELECT SCOPE_IDENTITY() AS Ident")
     stmt.execute
     id_value = stmt.rows.first["Ident"]
     # require 'pp'
     # pp stmt.rows
     # pp stmt.columns
-    # assert_equal(4, id_value, "inserted record's ID should match")
-    
-    # test bigint
-    connection.statement('create table bigint_test ( id bigint )').execute
+    assert_equal(4, id_value, "inserted record's ID should match")
+  end
+  
+  def test_bigint
+    @connection.statement('create table bigint_test ( id bigint )').execute
     bnum  = 9_000_000_000_000_000_000
-    connection.statement("insert into bigint_test ( id ) VALUES ( #{bnum} )").execute
-    stmt = connection.statement('select * from bigint_test')
+    @connection.statement("insert into bigint_test ( id ) VALUES ( #{bnum} )").execute
+    stmt = @connection.statement('select * from bigint_test')
     stmt.execute
     assert_equal bnum, stmt.rows.first["id"], "bigints should be equal"
   end
   
   def test_bad_connection
-    driver = FreeTDS::Driver.new
-    assert_not_nil(driver, "driver creation failed")
-    
     # these don't hit the server
-    assert_raise(ArgumentError) { driver.connect({}) }
+    assert_raise(ArgumentError) { @driver.connect({}) }
     
-    assert_raise(IOError) { driver.connect({:servername => 'beast', :username => 'xxxx'}) }
+    assert_raise(IOError) { @driver.connect({:servername => 'beast', :username => 'xxxx'}) }
   end
 
 
